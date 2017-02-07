fc24b7c74956db0a6fa4d4b830d3b76cc5dc84fd
Elliot Barkis <e.cbarkis@gmail.com>
2017-02-06 18:34:26 -0800

    Finished AssignmentOne

---
+++ AssignmentOne/declare.js
@@ -0,0 +1,27 @@
+var num1 = 83;
+console.log("The first number: ", num1);
+
+var num2 = 21;
+console.log("The second number: ", num2);
+
+var num3 = -8;
+console.log("The third number: ", num3);
+var num4 = 19.83;
+console.log("The fourth number: ", num4);
+
+var string1 = 'I like to use objects.';
+console.log("The first string: ", string1);
+var string2 = 'You can use single or double quotes for strings.';
+console.log("The second string: ", string2);
+
+var string3 = 'I like strings too.';
+console.log("The third string: ", string3);
+
+var bool1 = true;
+console.log("The first boolean: ", bool1);
+
+var bool2 = false;
+console.log("The second boolean: ", bool2);
+
+var define;
+console.log("The undefined variable: ", define)
