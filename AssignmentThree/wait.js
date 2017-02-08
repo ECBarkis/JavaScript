8c2c2e4fc244a506c4fda13ca13dfc2af0a3c63b
Elliot Barkis <e.cbarkis@gmail.com>
2017-02-08 13:06:38 -0800

    Starting AssignmentThree

---
+++ AssignmentThree/wait.js
@@ -0,0 +1,10 @@
+var daysUntilMyBirthday = 60;
+
+for (var i = daysUntilMyBirthday; i > 0; i--) {
+	if (i <= 5) {
+		console.log(i, "DAYS UNTIL MY BIRTHDAY!!!");
+	} else {
+		console.log(i, "days until my birthday. Such a long time... :(")
+	}
+}
+console.log("Happy Birthday!!!");
