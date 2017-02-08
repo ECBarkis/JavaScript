b1d9a6a7ae936189fd0b1ccabb67c5b0ea78c565
Elliot Barkis <e.cbarkis@gmail.com>
2017-02-07 16:43:46 -0800

    Starting AssignmentThree

--- AssignmentTwo/time.js
+++ AssignmentTwo/time.js
@@ -38,21 +38,6 @@ if (MINUTE > 30) {
 
 // Creating functionality by distingushing differnet times of day
 
-if (MINUTE > 30) {
-	HOUR ++;
-	if (PERIOD == "AM") {
-		console.log("It's almost", HOUR, "in the morning");
-	} else {
-		console.log("It's almost", HOUR, "in the evening");
-	}
-} else {
-	if (PERIOD == "AM") {
-		console.log("It's just after", HOUR, "in the morning");
-	} else {
-		console.log("It's just after", HOUR, "in the evening");
-	}
-}
-
 if (MINUTE == 45) {
 	HOUR ++;
 	if (PERIOD == "AM") {
